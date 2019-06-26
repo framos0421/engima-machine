81f6ced9c2ca6b7fcdd6c68b63882f57eee123b3
framos0421 <framos0421@berkeley.edu>
2019-06-25 21:10:20 -0700

    new submission

--- lab01/LeapYear.java
+++ lab01/LeapYear.java
@@ -34,10 +34,10 @@ public class LeapYear {
     */
     public static boolean isLeapYear(int year) {
         if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)){
-            return true
+            return true;
         }
         else {
-            return false
+            return false;
         }
     }
 }
--- lab02/Account.java
+++ lab02/Account.java
@@ -5,10 +5,17 @@
 public class Account {
 
     public int balance;
+    public Account parentAccount = new(Account)
 
     /** Initialize an account with the given balance. */
     public Account(int balance) {
         this.balance = balance;
+        this.parentAccount = null;
+    }
+
+    public Account(int balance, Account parentAccount) {
+        this.balance = balance;
+        this.parentAccount = parentAccount
     }
 
     /** Deposits amount into the current account. */
@@ -25,14 +32,21 @@ public class Account {
      * would leave a negative balance, print an error message and leave the
      * balance unchanged.
      */
-    public void withdraw(int amount) {
+    public boolean withdraw(int amount) {
         // TODO
         if (amount < 0) {
             System.out.println("Cannot withdraw negative amount.");
+            return false;
         } else if (balance < amount) {
+            if (this.parentAccount != null){
+                return parentAccount.withdraw(amount - balance)
+            }
+            else (this.parentAccount = null);
             System.out.println("Insufficient funds");
+            return false;
         } else {
             balance -= amount;
+            return true; 
         }
     }
 
@@ -42,5 +56,10 @@ public class Account {
      */
     public void merge(Account other) {
         // TODO
+        int amount = other.balance; 
+        this.deposit(amount);
+        other.withdraw(amount);
+
+
     }
 }

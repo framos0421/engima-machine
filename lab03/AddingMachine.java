fd40e21c7765ef05fbce724b4504fda77ec477fa
framos0421 <framos0421@berkeley.edu>
2019-06-27 20:34:23 -0700

    lab 4

--- lab03/AddingMachine.java
+++ lab03/AddingMachine.java
@@ -3,40 +3,36 @@ import java.util.*;
 public class AddingMachine {
 
 	public static void main (String[] args) {
-
 		Scanner scanner = new Scanner(System.in);
-		boolean isPreviousZero = false;
+		boolean justStarting = true;
 		int total = 0;
 		int subtotal = 0;
 		int input;
 		int last = 1;
 		int MAXIMUM_NUMBER_OF_INPUTS = 100;
-        int[] listOfInputs = new int[MAXIMUM_NUMBER_OF_INPUTS];
-        int index = 0;
-
-        // TODO Add code anywhere below to complete AddingMachine
+		int [] values = new int[MAXIMUM_NUMBER_OF_INPUTS];
+		int i = 0;
 		while (true) {
 			input = scanner.nextInt();
 			if (input == 0) {
-				if (isPreviousZero) {
+				if (last == 0) {
 					System.out.println("total " + total);
-					for (int i = 0; i <= index; i += 1){
-						System.out.println(listOfInputs[i]);
+					for(int j = 0;j <= i; j++){
+						System.out.println(values[j]);
 					}
 					return;
-				} else {
-					System.out.println("subtotal " + subtotal);
-					total += subtotal;
-					subtotal = 0;
-					isPreviousZero = true;
 				}
+				System.out.println("subtotal " + subtotal);
+				total += subtotal;
+				subtotal = 0;
 			}
 			subtotal += input;
-			last = 1;
-			if (input != 0) {
-				isPreviousZero = false;
+			last = input;
+			if(input != 0){
+				values[i] = input;
+				i++;
 			}
-
+			// TODO Your code here
 		}
 	}
 
